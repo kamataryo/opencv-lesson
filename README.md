2685ffc8c916a8c115b71db59de00223ebaa4717
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2017-11-12 15:51:27 +0900

    Init remo

---
+++ README.md
@@ -0,0 +1,7 @@
+# open cv test
+
+## installation
+
+```
+$ brew install opencv
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='opencv-lesson',
+      version='0.0',
+      description='my open cv lesson',
+      author='Kamata Ryo',
+      author_email='',
+      url='https://kamatatyo.com',
+      packages=[''],
+     )
